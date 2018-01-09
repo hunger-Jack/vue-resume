e23361f2bb18f68a0de729a77abef6351277431e
jack <2959921118@qq.com>
2018-01-10 02:40:52 +0800

    打包build

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-resume/dist',
+    assetsPublicPath: '/vue-resume/dist/',
 
     /**
      * Source Maps
