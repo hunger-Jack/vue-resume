6dc4e2e9f908cfd1c89ec3c730467d050679b847
jack <2959921118@qq.com>
2018-01-19 03:52:31 +0800

    使用了md5

--- src/lib/leancloud.js
+++ src/lib/leancloud.js
@@ -1,7 +1,7 @@
 import AV from 'leancloud-storage'
 
 var APP_ID = 'ALRLVlqlCdTrEnYOTyknP8h9-gzGzoHsz';
-var APP_KEY = 'e4VyeLQBhbrV6bJLpsuGhpiJ';
+var APP_KEY = 'ddaa4ae79b03c285d040b86784acdbdc';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
