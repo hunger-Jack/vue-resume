3a3439b13233b89ec157151134a34504271ba5c5
jack <2959921118@qq.com>
2018-01-19 23:54:41 +0800

    delete cache

--- src/lib/getAvUser.js
+++
@@ -1,9 +0,0 @@
-/*
-获取当前用户的信息
-*/
-import AV from './leancloud'
-
-export default function(user) {
-    var {id, attributes:{username}} = user || AV.User.current() || {attributes:{}}
-    return {id, username}
-  }
